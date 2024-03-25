42cf96f4fc5888ea81204332c0bd9ce0adf91ae2
Kürşad Yaprak <100860710+KursadYaprak@users.noreply.github.com>
2024-03-25 10:37:37 +0300

    Update WebConfig.java

--- src/main/java/dev/patika/VeterinaryManagementSystem/config/WebConfig.java
+++ src/main/java/dev/patika/VeterinaryManagementSystem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","veterinary-frontend-gamma.vercel.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
